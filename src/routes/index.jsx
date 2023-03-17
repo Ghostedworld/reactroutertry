2e57f141f26da1e71965ffb8bfe670e96ea4335f
Bolaji <borlarjhii@gmail.com>
2023-03-17 11:56:47 +0100

    Initial Commit

---
+++ src/routes/index.jsx
@@ -0,0 +1,11 @@
+export default function Index() {
+    return (
+      <p id="zero-state">
+        Hellllllllllp! My page is empty!
+        Hellllllllllp! My page is empty!
+        Hellllllllllp! My page is empty!
+        <br />
+        And Yes, I was created with react router
+      </p>
+    );
+  }
